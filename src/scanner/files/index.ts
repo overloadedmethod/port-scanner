2a72790af6f8ab3c1938595f24705684e0aef93c
overloadedmethod <overloadedmethod@gmail.com>
2020-11-14 16:51:21 +0200

    Added file reader + test

--- src/scanner/files/index.ts
+++ src/scanner/files/index.ts
@@ -1,15 +1,20 @@
+import fs from "fs";
+import readline from "readline";
+import path from "path";
+
 import { ResultRecord } from "../types";
 import { textFormatter } from "../utils";
 
-export async function* readFile(path: string) {
-  const lines = [
-    "localhost 8080",
-    "duckduckgo.com 80",
-    "duckduckgo.com 443",
-    "localhost 22",
-    "duckduckgo.com 9999",
-  ];
-  for (const line of lines) yield line;
+export async function* readFile(relPath: string) {
+  const filePath = path.join(relPath);
+  const fileStream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  for await (const line of rl) yield line;
 }
 
 export async function serializeScanProb(
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -2,164 +2,7 @@
 
 import utility from "../scanner";
 import { resolveIp, testPort } from "../scanner/network";
-
-// type ResultRecord = {
-//   ip: string;
-//   ports: number[];
-//   host: string;
-//   pinged: Record<number, number>;
-// };
-
-// const addressErrorLogger = (
-//   address: string,
-//   stage: string,
-//   error: Error,
-//   emptyResult: () => any = () => null
-// ) => {
-//   console.log(
-//     `unexpected happened with ${address} on ${stage} reason is ${error}`
-//   );
-//   return emptyResult();
-// };
-
-// async function ipResolveCall(host: string) {
-//   await new Promise((r) => setTimeout(r, faker.random.number(10000)));
-//   return Promise.resolve(faker.internet.ip());
-// }
-
-// async function portScanCall(address: string) {
-//   const start = Date.now();
-//   await new Promise((r) => setTimeout(r, faker.random.number(6000)));
-//   return Date.now() - start;
-// }
-
-// async function* readFile(path: string) {
-//   const lines = [
-//     "localhost 8080",
-//     "duckduckgo.com 80",
-//     "duckduckgo.com 443",
-//     "localhost 22",
-//     "duckduckgo.com 9999",
-//   ];
-//   for (const line of lines) yield line;
-// }
-
-// const line2UrlPortTpl = (hostPort: string): [string, number] => {
-//   const [host, port] = hostPort.split(" ");
-//   return [host, Number.parseInt(port)];
-// };
-
-// async function* parseLines(
-//   lines: ReturnType<typeof readFile>,
-//   parse = line2UrlPortTpl
-// ) {
-//   for await (const line of lines) yield parse(line);
-// }
-
-// async function* groupPortsByHost(hostPorts: ReturnType<typeof parseLines>) {
-//   const grouping: Record<string, number[]> = {};
-//   for await (const [host, port] of hostPorts)
-//     if (grouping[host]) grouping[host].push(port);
-//     else grouping[host] = [port];
-//   for await (const entry of Object.entries(grouping)) yield entry;
-// }
-
-// function promiseWithTimeout<T>(
-//   promise: () => Promise<T>,
-//   timeout = 20000,
-//   failureMessage = "timeout"
-// ) {
-//   let timeoutHandle: NodeJS.Timeout;
-//   const timeoutPromise = new Promise<never>((_, rej) => {
-//     timeoutHandle = setTimeout(() => rej(new Error(failureMessage)), timeout);
-//   });
-
-//   return Promise.race([promise(), timeoutPromise]).then((result) => {
-//     clearTimeout(timeoutHandle);
-//     return result;
-//   });
-// }
-
-// async function resolveIp(
-//   host: string,
-//   timeout = 4000,
-//   resolve = ipResolveCall,
-//   handleError = addressErrorLogger
-// ): Promise<string> {
-//   try {
-//     return await promiseWithTimeout(() => resolve(host), timeout);
-//   } catch (e) {
-//     return handleError(host, "resolving ip", e, () => null);
-//   }
-// }
-
-// async function testPort(
-//   address: string,
-//   timeout = 2000,
-//   scanPort = portScanCall,
-//   handleError = addressErrorLogger
-// ): ReturnType<typeof scanPort> {
-//   try {
-//     return await promiseWithTimeout(() => scanPort(address), timeout);
-//   } catch (e) {
-//     return handleError(address, "testing port", e, () => -1);
-//   }
-// }
-
-// async function* scanPorts(
-//   hosts: ReturnType<typeof groupPortsByHost>,
-//   ipResolver = resolveIp,
-//   portTester = testPort
-// ): AsyncGenerator<ResultRecord> {
-//   for await (const [host, ports] of hosts) {
-//     const ip: string = await ipResolver(host);
-//     if (!ip) {
-//       yield { ip, ports, host, pinged: [] };
-//       continue;
-//     }
-
-//     const pings = await Promise.all(
-//       ports.map((port) => portTester(`${ip}:${port}`))
-//     );
-
-//     const pinged: Record<number, number> = {};
-//     ports.forEach((port, index) => {
-//       if (pings[index] != -1) pinged[port] = pings[index];
-//     });
-
-//     yield { ip, ports, host, pinged };
-//   }
-// }
-
-// const textFormatter = ({ ip, host, ports, pinged }: ResultRecord) => {
-//   return `host:${host}\tip:${ip}\nscanned:${ports.join(
-//     ",\t"
-//   )}\npinged:\n\t${Object.entries(pinged)
-//     .map(([port, ping]) => `${port}:${ping}`)
-//     .join("\n\t")}`;
-// };
-
-// async function serializeScanProb(
-//   path: string,
-//   records: ReturnType<typeof scanPorts>,
-//   isJson = true,
-//   formatter = textFormatter
-// ) {
-//   if (isJson) {
-//     const result: Record<string, ResultRecord> = {};
-//     for await (const line of records) result[line.host] = line;
-//     console.log(result);
-//     return;
-//   }
-
-//   for await (const line of records) console.log(formatter(line));
-// }
-
-// async function utility(input: string, output: string, isJSON: boolean) {
-//   const lines = parseLines(readFile(input));
-//   const results = scanPorts(groupPortsByHost(lines));
-//   await serializeScanProb(output, results, isJSON);
-// }
+import { readFile } from "../scanner/files";
 
 jest.setTimeout(30000);
 
@@ -215,5 +58,28 @@ describe("Port scanning", () => {
       Promise.reject(new Error("wrong something"))
     );
     expect(result).toBe(-1);
+  });
+});
+
+describe("file utilities", () => {
+  it("should parse provided file of test.txt", async () => {
+    const lines = [];
+    const expected = [
+      "localhost 8080",
+      "duckduckgo.com 80",
+      "duckduckgo.com 443",
+      "localhost 22",
+      "duckduckgo.com 9999",
+    ];
+    for await (const line of readFile("./src/test/test.txt")) lines.push(line);
+    expect(lines).toEqual(expected);
+  });
+
+  it("should serialize provided data as json", async () => {
+    throw new Error("not implemented");
+  });
+
+  it("should serialize provided data as text", async () => {
+    throw new Error("not implemented");
   });
 });
